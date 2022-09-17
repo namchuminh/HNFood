3e92b0c2876749db84291b8558bcbca9ee8f5b08
Ngoc Ha <hacan2k2@gmail.com>
2022-09-17 17:48:27 +0700

    add screen list food

--- screens/index.js
+++ screens/index.js
@@ -2,9 +2,11 @@ import Welcome from "./Welcome.js"
 import Intro from "./Intro.js"
 import Login from "./Login.js"
 import Register from './Register'
+import Home from "./Home.js"
 export{
     Welcome,
     Intro,
     Login,
     Register,
+    Home,
 }
