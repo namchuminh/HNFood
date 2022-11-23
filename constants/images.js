43b10f0d44121126a826a3e2898005773dd7c6ad
namchuminh <chuminhnamma@gmail.com>
2022-11-23 23:03:15 +0700

    fix image

--- constants/images.js
+++ constants/images.js
@@ -3,24 +3,8 @@ const welcome_image2 = require('../assets/welcome_image2.png')
 const fast_food = require('../assets/fast_food.png')
 const logo_food = require('../assets/logo_food.png')
 const logo_sigup =  require('../assets/logo_signup.png')
-const hamburger =  require('../assets/hamburger.png')
-const pizza =  require('../assets/pizza.jpg')
-const suachua =  require('../assets/suachua.jpg')
-const kem =  require('../assets/kem.png')
-const mochi =  require('../assets/mochi.jpg')
-const chocolate =  require('../assets/chocolate.jpg')
-const cakes =  require('../assets/cakes.png')
-const drinks =  require('../assets/drinks.jpg')
-const foods =  require('../assets/foods.jpg')
-const fruits =  require('../assets/fruits.jpg')
-const vegetables =  require('../assets/vegetables.jpg')
 const banner =  require('../assets/banner.jpg')
-const keo =  require('../assets/keo.jpg')
-const noodles =  require('../assets/noodles.jpg')
-const noodles2 =  require('../assets/noodles2.png')
-const noodles3 =  require('../assets/noodles3.jpg')
-const avatar =  require('../assets/avatar.png')
-const nuoccam =  require('../assets/nuoccam.jpg')
+
 
 export default {
     welcome_image,
@@ -28,22 +12,5 @@ export default {
     fast_food,
     logo_food,
     logo_sigup,
-    hamburger,
-    pizza,
-    suachua,
-    kem,
-    mochi,
-    chocolate,
-    cakes,
-    drinks,
-    foods,
-    fruits,
-    vegetables,
     banner,
-    keo,
-    noodles,
-    noodles2,
-    noodles3,
-    avatar,
-    nuoccam,
 }
